83b8c2dbaf468baf0fc4f394763c358543794d55
Your Name <you@example.com>
2023-09-13 22:14:05 +0530

    added html smooth attribute

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,19 @@
-import './Sidebar.css';
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-elements">
-      <h1> Columbus </h1>
+        <h1> Columbus </h1>
 
-      <div className='sections'>
-        <div>PMS</div>
-        <div>DSP</div>
-        <div>Stock +</div>
-        <div>MD</div>
-
+        <div className="sections">
+          <div> PMS </div>
+          <div> DSP </div>
+          <div> Stock + </div>
+          <div> MD </div>
+          <div> ADDD </div>
+        </div>
       </div>
-
-      </div>
-
     </div>
   );
 }
